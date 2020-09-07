8aa1bb10950304ab6e0f002218a5ae0ce260ba76
Vlad Anisimov <anisimov@dbosoft.com.ua>
2020-09-07 17:38:28 +0300

    Initial Commit

---
+++ app/src/main/java/com/anisimov/vlad/apitest/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.anisimov.vlad.apitest.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anisimov.vlad.apitest.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
