a365fab927fbca6ec007d24ee2d437fe1087facd
Vlad Anisimov <anisimov@dbosoft.com.ua>
2020-09-10 15:17:03 +0300

    Rename activity Implement activity

---
+++ app/src/main/java/com/anisimov/vlad/apitest/ui/activity/RepoListActivity.kt
@@ -0,0 +1,94 @@
+package com.anisimov.vlad.apitest.ui.activity
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.anisimov.vlad.apitest.R
+import com.anisimov.vlad.apitest.domain.model.RepoUI
+import com.anisimov.vlad.apitest.domain.viewmodel.RepoListViewModel
+import eu.davidea.flexibleadapter.FlexibleAdapter
+import eu.davidea.flexibleadapter.items.AbstractFlexibleItem
+import eu.davidea.flexibleadapter.items.IFlexible
+import eu.davidea.viewholders.FlexibleViewHolder
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class RepoListActivity : BaseActivity<RepoListViewModel>() {
+
+    override fun provideViewModelClass(): Class<RepoListViewModel> = RepoListViewModel::class.java
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupRepoList()
+    }
+
+    @SuppressLint("Range")
+    private fun setupRepoList() {
+        val layoutManager = LinearLayoutManager(this)
+        rvRepoList.layoutManager = layoutManager
+        val adapter = FlexibleAdapter<RepoAdapterItem>(ArrayList())
+        rvRepoList.addItemDecoration(
+            DividerItemDecoration(
+                rvRepoList.context,
+                layoutManager.orientation
+            )
+        )
+        rvRepoList.adapter = adapter
+        viewModel.oNewReposEvent.observe(this) { newReposEvent ->
+            if (newReposEvent.newSearch) {
+                adapter.clear()
+            }
+            val adapterItems = newReposEvent.repos.map { RepoAdapterItem(it) }
+            adapter.addItems(-1, adapterItems)
+        }
+        viewModel.search("tetris")
+    }
+
+    class RepoAdapterItem(private val repo: RepoUI) :
+        AbstractFlexibleItem<RepoAdapterItem.RepoViewHolder>() {
+
+        override fun equals(other: Any?): Boolean {
+            if (other is RepoAdapterItem) {
+                return other.repo.id == repo.id
+            }
+            return false
+        }
+
+        override fun hashCode(): Int {
+            return repo.id
+        }
+
+        override fun getLayoutRes(): Int {
+            return R.layout.repo_list_item
+        }
+
+        override fun createViewHolder(
+            view: View,
+            adapter: FlexibleAdapter<IFlexible<*>?>?
+        ): RepoViewHolder {
+            return RepoViewHolder(view, adapter)
+        }
+
+        override fun bindViewHolder(
+            adapter: FlexibleAdapter<IFlexible<*>?>?, holder: RepoViewHolder,
+            position: Int,
+            payloads: List<Any>
+        ) {
+            holder.tvRepoName.text = repo.name
+            holder.tvRepoDescription.text = repo.description
+        }
+
+        class RepoViewHolder(view: View, adapter: FlexibleAdapter<*>?) :
+            FlexibleViewHolder(view, adapter) {
+            val tvRepoName: TextView = view.findViewById(R.id.tvRepoName)
+            val tvRepoDescription: TextView = view.findViewById(R.id.tvRepoDescription)
+
+        }
+    }
+
+
+}
