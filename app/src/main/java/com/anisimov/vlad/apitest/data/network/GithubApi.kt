ca0cb0344607609c56daf6f4bd63db69739b93c5
Vlad Anisimov <anisimov@dbosoft.com.ua>
2020-09-10 13:40:44 +0300

    Optimize request

--- app/src/main/java/com/anisimov/vlad/apitest/data/network/GithubApi.kt
+++ app/src/main/java/com/anisimov/vlad/apitest/data/network/GithubApi.kt
@@ -1,5 +1,6 @@
 package com.anisimov.vlad.apitest.data.network
 
+import com.anisimov.vlad.apitest.data.model.network.SearchResponseNetwork
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -8,7 +9,7 @@ interface GithubApi {
     @GET("search/repositories")
     suspend fun getRepositories(
         @Query("q") query: String,
-        @Query("page") page: String,
+        @Query("page") page: Int,
         @Query("per_page") perPage: Int
-    )
+    ) : SearchResponseNetwork
 }
