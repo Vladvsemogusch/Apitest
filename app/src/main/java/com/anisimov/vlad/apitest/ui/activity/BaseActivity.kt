a4e3410b40441ffcae436f5b29f54847ce18e6d4
Vlad Anisimov <anisimov@dbosoft.com.ua>
2020-09-10 15:16:11 +0300

    Fix visibility in base activity

--- app/src/main/java/com/anisimov/vlad/apitest/ui/activity/BaseActivity.kt
+++ app/src/main/java/com/anisimov/vlad/apitest/ui/activity/BaseActivity.kt
@@ -4,10 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
 
 abstract class BaseActivity<VM : AndroidViewModel> : AppCompatActivity(){
-    private lateinit var viewModel: VM
+    protected lateinit var viewModel: VM
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
