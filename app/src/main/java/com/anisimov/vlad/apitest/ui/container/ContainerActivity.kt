542cb76fb09859d6ee4528d8373fb7b4a73b3a2b
Vladyslav <anisimov.vlad@gmail.com>
2020-09-11 21:49:50 +0300

    Finish new navigation support

--- app/src/main/java/com/anisimov/vlad/apitest/ui/container/ContainerActivity.kt
+++ app/src/main/java/com/anisimov/vlad/apitest/ui/container/ContainerActivity.kt
@@ -2,6 +2,7 @@ package com.anisimov.vlad.apitest.ui.container
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.anisimov.vlad.apitest.R
 
@@ -13,7 +14,10 @@ class ContainerActivity : AppCompatActivity() {
         setContentView(R.layout.main_container)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        return true
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home){
+            onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
